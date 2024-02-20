439bb44e6b4a215e38ce491bb88d7ba740e5eddd
Sanmi Akinwunmi <sanmi.hq@gmail.com>
2024-02-20 02:53:30 +0100

    add: connect to redis & store url mappings

---
+++ lib/redis.ts
@@ -0,0 +1,7 @@
+import { Redis } from "@upstash/redis";
+
+export const redis = new Redis({
+  url: "https://certain-oriole-43380.upstash.io",
+  token:
+    "Aal0ASQgMmExZmEyOWYtMGQzMC00NmE0LTg0YmMtMjZmYjQ0NTczYmFmMTJmOTRkYTBkMDBkNGM4ZThhNTBlNGQ4ZTZiMTIwOTY=",
+});
--- src/app/api/shorten/route.ts
+++ src/app/api/shorten/route.ts
@@ -1,11 +1,19 @@
 import { nanoid } from "nanoid";
 import { NextRequest, NextResponse } from "next/server";
+import { redis } from "../../../../lib/redis";
 
 export async function POST(req: NextRequest, res: NextResponse) {
   try {
+    // get longUrl from request
     const { longUrl } = await req.json();
+
+    // generate shortUrl
     const shortId = nanoid(10);
     const shortUrl = `zip.link/${shortId}`;
+
+    // save it to redis using hset
+    await redis.hset("urls", { [shortUrl]: longUrl });
+
     const responsePayload = { shortUrl };
 
     return new Response(JSON.stringify(responsePayload), {
